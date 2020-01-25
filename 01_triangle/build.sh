db0293f5324e1a24122130fc527a12f9700142d6
Kion <kion@dashgl.com>
2020-01-25 03:53:02 +0000

    gitignore

---
+++ 01_triangle/build.sh
@@ -0,0 +1,2 @@
+gcc -c main.c -o tmp.o -I/opt/vc/include/ -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -I./ -I/opt/vc/src/hello_pi/libs/ilclient -I/opt/vc/src/hello_pi/libs/vgfont -I/opt/vc/src/hello_pi/libs/revision
+gcc -o a.out tmp.o -L/opt/vc/lib/ -lbrcmGLESv2 -lbrcmEGL -lopenmaxil -lbcm_host -lvcos -lvchiq_arm -lpthread -lrt -lm -L/opt/vc/src/hello_pi/libs/ilclient -L/opt/vc/src/hello_pi/libs/vgfont -L/opt/vc/src/hello_pi/libs/revision
